1e251de79bbf93e1a7c13ae14bdbe6ed3f3ee966
Naziya Kadri <nazzkadri@MacBook-Pro-67.local>
2018-09-04 15:40:06 +0100

    passed the first unit test

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,7 @@
+require 'docking_station.rb'
+describe DockingStation do
+  # it "Docking station to release the bike" do
+  #   docking_station = DockingStation
+  #   expect docking_station.release_bike.to eq ("true")
+  # end
+end
