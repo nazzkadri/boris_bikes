e721972c7cad1aa8d2864a2702ae36ab4b8f794e
Naziya Kadri <nazzkadri@MacBook-Pro-67.local>
2018-09-04 16:58:43 +0100

    back to the unit - test 8

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,11 @@
 require 'docking_station.rb'
+
 describe DockingStation do
-  # it "Docking station to release the bike" do
-  #   docking_station = DockingStation
-  #   expect docking_station.release_bike.to eq ("true")
-  # end
+#it {is_expected.to respond_to :release_bike}
+  describe 'release_bike' do
+  it "Docking station to release the bike" do
+    docking_station = DockingStation.new
+    expect(docking_station).to respond_to :release_bike
+  end
+end
 end
