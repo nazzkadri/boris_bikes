ff2012a8dc15064a977c562c880fb52633d73c0d
Naziya Kadri <nazzkadri@MacBook-Pro-67.local>
2018-09-04 17:00:10 +0100

    one liner syntax - test 8

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,6 @@
 require 'docking_station.rb'
 
 describe DockingStation do
-#it {is_expected.to respond_to :release_bike}
-  describe 'release_bike' do
-  it "Docking station to release the bike" do
-    docking_station = DockingStation.new
-    expect(docking_station).to respond_to :release_bike
-  end
-end
+it {is_expected.to respond_to :release_bike}
+  
 end
